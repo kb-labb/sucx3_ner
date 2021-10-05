23c44b0b3a05a44edc6688893348065921482260
Robin Kurtz <robin.kurtz@kb.se>
2021-10-05 10:36:44 +0200

    download suc and prepare datasets script

---
+++ download_and_convert_suc3.sh
@@ -0,0 +1,69 @@
+#! /usr/bin/bash
+
+# Data directory
+data="./data"
+mkdir  $data
+mkdir $data/raw
+
+# Download SUC 3.0
+wget http://spraakbanken.gu.se/lb/resurser/meningsmangder/suc3.xml.bz2 -P $data/raw
+tar xjvf $data/raw/suc3.xml.bz2 -C $data/raw
+
+# convert xml to conll format
+mkdir $data/base
+OT="original_tags"
+ST="simple_tags"
+STNMT="simple_tags.no_MSR_TME"
+python suc_handler.py --infile $data/raw/suc3.xml --outfile $data/base/suc3.$OT.conll --original_tags
+python suc_handler.py --infile $data/base/suc3.$OT.conll --outfile $data/base/suc3.$OT.jsonl --conllu2json
+
+python suc_handler.py --infile $data/raw/suc3.xml --outfile $data/base/suc3.$ST.conll
+python suc_handler.py --infile $data/base/suc3.$ST.conll --outfile $data/base/suc3.$ST.jsonl --conllu2json
+
+python suc_handler.py --infile $data/raw/suc3.xml --outfile $data/base/suc3.$STNMT.conll --no_MSR_TME
+python suc_handler.py --infile $data/base/suc3.$STNMT.conll --outfile $data/base/suc3.$STNMT.jsonl --conllu2json
+
+# create canonical split based on original tag distribution
+mkdir $data/$OT
+python split_and_filter.py --infile $data/base/suc3.jsonl  --outfolder $data/$OT --seed 12345
+
+
+# recreate canonical split based on original tag distribution's split_ids
+mkdir $data/$ST
+python split_and_filter.py --infile $data/base/suc3.$ST.jsonl  --outfolder $data/$ST --split_ids $data/$OT/split_ids.json
+
+mkdir $data/$STNMT
+python split_and_filter.py --infile $data/base/suc3.$STNMT.jsonl  --outfolder $data/$STNMT --split_ids $data/$OT/split_ids.json
+
+# lowercase all or only NE
+for i in $OT $ST $STNMT;
+do
+    F=$data/$i;
+
+    x="lower"
+    mkdir $F/$x
+    T=$F/$x;
+    python lowercase_ner_data.py --infiles $F/train.jsonl $F/dev.jsonl $F/test.jsonl --outfiles $T/train.jsonl $T/dev.jsonl $T/test.jsonl;
+
+    mkdir $F/${x}_mix
+    T=$F/${x}_mix;
+    python lowercase_ner_data.py --infiles $F/train.jsonl $F/dev.jsonl $F/test.jsonl --outfiles $T/train.jsonl $T/dev.jsonl $T/test.jsonl --mix --seed 12345;
+
+    mkdir $F/${x}_both
+    T=$F/${x}_both;
+    python lowercase_ner_data.py --infiles $F/train.jsonl $F/dev.jsonl $F/test.jsonl --outfiles $T/train.jsonl $T/dev.jsonl $T/test.jsonl --both;
+
+    x="ne_lower"
+    mkdir $F/$x
+    T=$F/$x;
+    python lowercase_ner_data.py --infiles $F/train.jsonl $F/dev.jsonl $F/test.jsonl --outfiles $T/train.jsonl $T/dev.jsonl $T/test.jsonl --ne_only;
+
+    mkdir $F/${x}_both
+    T=$F/ne_lower_both;
+    python lowercase_ner_data.py --infiles $F/train.jsonl $F/dev.jsonl $F/test.jsonl --outfiles $T/train.jsonl $T/dev.jsonl $T/test.jsonl --ne_only --both;
+
+    mkdir $F/${x}_mix
+    T=$F/${x}_mix;
+    python lowercase_ner_data.py --infiles $F/train.jsonl $F/dev.jsonl $F/test.jsonl --outfiles $T/train.jsonl $T/dev.jsonl $T/test.jsonl --ne_only --mix --seed 12345;
+done
+
--- lowercase_ner_data.py
+++ lowercase_ner_data.py
@@ -1,52 +1,73 @@
 import json
 import argparse
-from typing import Iterable, Dict, List, Union
+import random
+from typing import Iterable, TypedDict, List
 
-A = Union[int, List[str]]
+Sentence = TypedDict("Sentence",
+                     {
+                        "id": str,
+                        "tokens": List[str],
+                        "pos_tags": List[str],
+                        "ner_tags": List[str],
+                     }
+                     )
 
 
-def lowercase_all_text(fn: str, both: bool) -> Iterable[Dict[str, A]]:
+def lowercase_all_text(fn: str, both: bool, mix: bool) -> Iterable[Sentence]:
     with open(fn) as fh:
-        i = 0
         for line in fh:
             jline = json.loads(line)
+            lower_jline = {k: v for k, v in jline.items()}  # type: Sentence
+            lower_jline["tokens"] = [token.lower() for token in jline["tokens"]]
+            lower_jline["id"] = jline["id"] + "_lower"
             if both:
-                jline["id"] = i
                 yield jline
-                i += 1
-            jline["tokens"] = [token.lower() for token in jline["tokens"]]
-            jline["id"] = i
-            yield jline
-            i += 1
+                yield lower_jline
+            elif mix:
+                if random.random() <= 0.5:
+                    yield jline
+                else:
+                    yield lower_jline
+            else:
+                yield lower_jline
 
 
-def lowercase_ne_only(fn: str, both: bool) -> Iterable[Dict[str, A]]:
+def lowercase_ne_only(fn: str, both: bool, mix: bool) -> Iterable[Sentence]:
     with open(fn) as fh:
-        i = 0
         for line in fh:
             jline = json.loads(line)
-            if both:
-                jline["id"] = i
-                yield jline
-                i += 1
             tokens = []
             for tag, token in zip(jline["ner_tags"], jline["tokens"]):
                 if tag != "O":
                     token = token.lower()
                 tokens.append(token)
-            jline["tokens"] = tokens
-            jline["id"] = i
-            yield jline
-            i += 1
+            lower_jline = {k: v for k, v in jline.items()}  # type: Sentence
+            lower_jline["tokens"] = tokens
+            lower_jline["id"] = jline["id"] + "_ne_lower"
+            if both:
+                yield jline
+                yield lower_jline
+            elif mix:
+                if random.random() <= 0.5:
+                    yield jline
+                else:
+                    yield lower_jline
+            else:
+                yield lower_jline
 
 
-def lowercase_wrapper(fn_in: str, ne_only: bool, both: bool, fn_out) -> None:
+def lowercase_wrapper(fn_in: str,
+                      ne_only: bool,
+                      both: bool,
+                      mix: bool,
+                      fn_out,
+                      ) -> None:
     with open(fn_out, "w") as fout:
         if ne_only:
-            for jline in lowercase_ne_only(fn_in, both):
+            for jline in lowercase_ne_only(fn_in, both, mix):
                 print(json.dumps(jline), file=fout)
         else:
-            for jline in lowercase_all_text(fn_in, both):
+            for jline in lowercase_all_text(fn_in, both, mix):
                 print(json.dumps(jline), file=fout)
 
 
@@ -56,15 +77,19 @@ def get_args() -> argparse.Namespace:
     parser.add_argument("--outfiles", nargs="+")
     parser.add_argument("--ne_only", action="store_true")
     parser.add_argument("--both", action="store_true")
+    parser.add_argument("--mix", action="store_true")
+    parser.add_argument("--seed", type=int)
     return parser.parse_args()
 
 
 def main() -> None:
     args = get_args()
+    if args.seed:
+        random.seed(args.seed)
     print(args)
     assert len(args.infiles) == len(args.outfiles)
     for i, o in zip(args.infiles, args.outfiles):
-        lowercase_wrapper(i, args.ne_only, args.both, o)
+        lowercase_wrapper(i, args.ne_only, args.both, args.mix, o)
 
 
 if __name__ == "__main__":
